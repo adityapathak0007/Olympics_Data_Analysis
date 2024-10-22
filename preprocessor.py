f42b7aa1eb0f68a8a8a3b5a15fab42321d69c2a5
Aditya Pathak <adityapathak034@gmail.com>
2024-10-22 11:33:07 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import numpy as np
 
+def preprocess(df, region_df):
+    # Check if 'Season' column exists
+    if 'Season' not in df.columns:
+        raise KeyError("The DataFrame does not contain the 'Season' column.")
 
-def preprocess(df,region_df):
-    # filtering for summer olympics
+    # Filtering for summer olympics
     df = df[df['Season'] == 'Summer']
-    # merge with region_df
-    df = df.merge(region_df, on='NOC', how='left')
-    # dropping duplicates
-    df.drop_duplicates(inplace=True)
-    # one hot encoding modals
-    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
-    return df
 
+    # Merge with region_df
+    df = df.merge(region_df, on='NOC', how='left')
+
+    # Dropping duplicates
+    df.drop_duplicates(inplace=True)
+
+    # One hot encoding medals
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+    
+    return df
